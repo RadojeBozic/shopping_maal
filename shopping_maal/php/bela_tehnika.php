6aaa4d1c1708c634ad83b874353468615d293472
RadojeBozic <150623205+RadojeBozic@users.noreply.github.com>
2024-12-29 13:39:41 +0100

    Add files via upload
    Signed-off-by: RadojeBozic <150623205+RadojeBozic@users.noreply.github.com>

---
+++ shopping_maal/php/bela_tehnika.php
@@ -0,0 +1,162 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bela tehnika</title>
+    <link rel="stylesheet" href="../css/style.css">
+</head>
+<body>
+    <div class="container-header">
+        <div class="header">
+            <div class="img-header">
+                <img src="../images/bela_tehnika.jpg" alt="logo">
+            </div>
+            <div class="text-header">
+                <h1>Prvi on - line šoping mol</h1>
+                <p>Dolazi do Vas!</p>
+            </div>
+            <div class="sign-in">
+                <img src="https://placeholder.com/150x150" alt="">
+            </div>
+        </div> <!-- end .header -->
+    </div> <!-- end .container-header -->
+    <div class="container-nav">
+        <div class="nav">
+    <ul>
+    <li><a href="index.php">Šoping mol</a>
+            <ul>
+            <li><a href="#">Podgrupa 1</a></li>
+            <li><a href="#">Podgrupa 2</a></li>
+            <li><a href="#">Podgrupa 3</a></li>
+            <li><a href="#">Podgrupa 4</a></li>
+            <li><a href="#">Podgrupa 5</a></li>
+            </ul>
+        </li>
+        <li><a href="voce_povrce.php">Voće i povrće</a>
+            <ul>
+            <li><a href="#">Podgrupa 1</a></li>
+            <li><a href="#">Podgrupa 2</a></li>
+            <li><a href="#">Podgrupa 3</a></li>
+            <li><a href="#">Podgrupa 4</a></li>
+            <li><a href="#">Podgrupa 5</a></li>
+            </ul>
+        </li>
+        <li><a href="prehrana.php">Prehrana</a>
+            <ul>
+            <li><a href="#">Podgrupa 1</a></li>
+            <li><a href="#">Podgrupa 2</a></li>
+            <li><a href="#">Podgrupa 3</a></li>
+            <li><a href="#">Podgrupa 4</a></li>
+            <li><a href="#">Podgrupa 5</a></li>
+            </ul>
+        </li>
+        <li><a href="zdrava_hrana.php">Zdrava hrana</a>
+            <ul>
+            <li><a href="#">Podgrupa 1</a></li>
+            <li><a href="#">Podgrupa 2</a></li>
+            <li><a href="#">Podgrupa 3</a></li>
+            <li><a href="#">Podgrupa 4</a></li>
+            <li><a href="#">Podgrupa 5</a></li>
+            </ul>
+        </li>
+        <li><a href="kompjuteri.php">Kompjuteri</a>
+            <ul>
+            <li><a href="#">Podgrupa 1</a></li>
+            <li><a href="#">Podgrupa 2</a></li>
+            <li><a href="#">Podgrupa 3</a></li>
+            <li><a href="#">Podgrupa 4</a></li>
+            <li><a href="#">Podgrupa 5</a></li>
+            </ul>
+        </li>
+        <li><a href="mobilni_telefoni.php">Mobilni telefoni</a>
+            <ul>
+            <li><a href="#">Podgrupa 1</a></li>
+            <li><a href="#">Podgrupa 2</a></li>
+            <li><a href="#">Podgrupa 3</a></li>
+            <li><a href="#">Podgrupa 4</a></li>
+            <li><a href="#">Podgrupa 5</a></li>
+            </ul>
+        </li>
+        <li><a href="garderoba.php">Garderoba</a>
+            <ul>
+            <li><a href="#">Podgrupa 1</a></li>
+            <li><a href="#">Podgrupa 2</a></li>
+            <li><a href="#">Podgrupa 3</a></li>
+            <li><a href="#">Podgrupa 4</a></li>
+            <li><a href="#">Podgrupa 5</a></li>
+            </ul>
+        </li>
+        <li><a href="bela_tehnika.php">Bela tehnika</a>
+            <ul>
+            <li><a href="#">Podgrupa 1</a></li>
+            <li><a href="#">Podgrupa 2</a></li>
+            <li><a href="#">Podgrupa 3</a></li>
+            <li><a href="#">Podgrupa 4</a></li>
+            <li><a href="#">Podgrupa 5</a></li>
+            </ul>
+        </li>
+        <li><a href="#">Grupa 8</a>
+            <ul>
+            <li><a href="#">Podgrupa 1</a></li>
+            <li><a href="#">Podgrupa 2</a></li>
+            <li><a href="#">Podgrupa 3</a></li>
+            <li><a href="#">Podgrupa 4</a></li>
+            <li><a href="#">Podgrupa 5</a></li>
+            </ul>
+        </li>
+        <li><a href="#">Grupa 9</a>
+            <ul>
+            <li><a href="#">Podgrupa 1</a></li>
+            <li><a href="#">Podgrupa 2</a></li>
+            <li><a href="#">Podgrupa 3</a></li>
+            <li><a href="#">Podgrupa 4</a></li>
+            <li><a href="#">Podgrupa 5</a></li>
+            </ul>
+        </li>
+        <li><a href="#">Grupa 10</a>
+            <ul>
+            <li><a href="#">Podgrupa 1</a></li>
+            <li><a href="#">Podgrupa 2</a></li>
+            <li><a href="#">Podgrupa 3</a></li>
+            <li><a href="#">Podgrupa 4</a></li>
+            <li><a href="#">Podgrupa 5</a></li>
+            </ul>
+        </li>
+        <!-- <li><a href="#">Grupa 11</a>
+            <ul>
+            <li><a href="#">Podgrupa 1</a></li>
+            <li><a href="#">Podgrupa 2</a></li>
+            <li><a href="#">Podgrupa 3</a></li>
+            <li><a href="#">Podgrupa 4</a></li>
+            <li><a href="#">Podgrupa 5</a></li>
+            </ul>
+        </li>
+        <li><a href="#">Grupa 12</a>
+            <ul>
+            <li><a href="#">Podgrupa 1</a></li>
+            <li><a href="#">Podgrupa 2</a></li>
+            <li><a href="#">Podgrupa 3</a></li>
+            <li><a href="#">Podgrupa 4</a></li>
+            <li><a href="#">Podgrupa 5</a></li>
+            </ul>
+        </li>  -->
+    </ul>
+
+        </div> <!-- end .nav -->
+    </div> <!-- end container-nav -->
+
+    <div class="container-section">
+        <div class="section">
+
+        </div> <!-- end .section -->
+    </div> <!-- end container-section -->
+        <div class="footer">
+            <p>
+                &copy; Mont Fruit doo Beograd - 2024 Sva prava zadržana
+            </p>
+        </div>
+    
+    
+</body>
+</html>
